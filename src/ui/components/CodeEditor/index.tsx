31dff0cd62e8679ce40286bb4ab1db6521991645
leoGeorges <leo@georges.tech>
2021-10-28 19:21:03 +0200

    :art:

--- src/ui/components/CodeEditor/index.tsx
+++ src/ui/components/CodeEditor/index.tsx
@@ -23,39 +23,3 @@ export default function CodeEditor() {
     />
   )
 }
-
-// import { useEffect, useRef } from "react";
-// import * as monaco from "monaco-editor";
-
-// monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-//   target: monaco.languages.typescript.ScriptTarget.ES2018,
-//   lib: ["es2015", "es2016", "es2017", "es2018"],
-//   allowNonTsExtensions: true,
-// });
-
-// export default function CodeEditor() {
-//   const editorContainer = useRef<HTMLDivElement>(null);
-//   let editor: monaco.editor.IStandaloneCodeEditor;
-
-//   useEffect(() => {
-//     if(!editorContainer.current) return;
-
-//     editor = monaco.editor.create(editorContainer.current, {
-//       value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
-//       language: "typescript",
-//       minimap: {
-//         enabled: false,
-//       },
-//       theme: "vs-dark",
-//     });
-//     return () => {
-//       editor.dispose();
-//     }
-//   }, []);
-
-//   return (
-//     <>
-//       <div ref={editorContainer} ></div>
-//     </>
-//   );
-// } 
