45da15123fea884d0bbf234c748413d69f1e8dd5
Maxime Chabert <sin2.chabert.maxime@gmail.com>
2021-11-07 11:21:56 +0100

    clean Layout component

--- src/ui/components/Layout/index.tsx
+++ src/ui/components/Layout/index.tsx
@@ -1,26 +1,7 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
 import { styled } from '@mui/material'
 import Box from '@mui/material/Box'
-import Drawer from '@mui/material/Drawer'
 import CssBaseline from '@mui/material/CssBaseline'
-import Toolbar from '@mui/material/Toolbar'
-import List from '@mui/material/List'
-import Divider from '@mui/material/Divider'
-import ListItem from '@mui/material/ListItem'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import StorageIcon from '@mui/icons-material/StorageOutlined'
-import SettingsIcon from '@mui/icons-material/SettingsOutlined'
-import AddIcon from '@mui/icons-material/AddOutlined'
-
-const drawerWidth = 60
-const colorIcon = '#858585'
-
-const Menu = styled(Drawer)(({ theme }) => ({
-  '.MuiDrawer-paper': {
-    backgroundColor: theme.palette.primary.main,
-  },
-}))
 
 const Content = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.secondary.main,
