8923ccb08f017e8af6acd6c2eb1aa56d7e44fcbe
Maxime Chabert <sin2.chabert.maxime@gmail.com>
2021-11-07 19:13:25 +0100

    update connection page style

--- src/ui/pages/ConnectionPage.tsx
+++ src/ui/pages/ConnectionPage.tsx
@@ -2,10 +2,15 @@ import * as React from 'react'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import LoadingButton from '@mui/lab/LoadingButton'
-import { Container, Grid, Paper, Typography } from '@mui/material'
+import { styled, Container, Paper, Typography, Divider } from '@mui/material'
 import { useMessager } from '../core/messager'
 
 const DEFAULT_CONNECTION_STRING = 'mongodb://localhost:27017'
+
+const FormContent = styled(Box)({
+  display: 'flex',
+  padding: 25,
+})
 
 export default function ConnectionPage() {
   const messager = useMessager()
@@ -30,33 +35,31 @@ export default function ConnectionPage() {
   return (
     <Container>
       <Paper>
-        <Box px={3} py={2} component="form" noValidate autoComplete="off">
-          <Typography variant="h6" margin="dense">
+        <Box component="form" noValidate autoComplete="off">
+          <Typography px={3} py={2} variant="h6">
             Add a new database
           </Typography>
 
-          <Grid container>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                placeholder={DEFAULT_CONNECTION_STRING}
-                label="Connection string"
-                variant="outlined"
-                value={connectionString}
-                onChange={event => updateConnectionString(event.target.value)}
-                fullWidth
-                margin="dense"
-              />
-            </Grid>
-          </Grid>
+          <Divider />
 
-          <LoadingButton
-            variant="contained"
-            size="large"
-            loading={isLoading}
-            onClick={connect}
-          >
-            Connect
-          </LoadingButton>
+          <FormContent>
+            <TextField
+              placeholder={DEFAULT_CONNECTION_STRING}
+              label="Connection string"
+              variant="outlined"
+              value={connectionString}
+              onChange={event => updateConnectionString(event.target.value)}
+              fullWidth
+            />
+              <LoadingButton
+                sx={{ marginLeft: '10px' }}
+                variant="outlined"
+                loading={isLoading}
+                onClick={connect}
+              >
+              Connect
+            </LoadingButton>
+          </FormContent>
         </Box>
       </Paper>
     </Container>
