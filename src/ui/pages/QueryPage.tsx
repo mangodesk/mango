9d22db334474ebc709ab361920a464a9fab67709
leoGeorges <leo@georges.tech>
2021-11-08 17:57:39 +0100

    Use List and Collapse for collections ui

--- src/ui/pages/QueryPage.tsx
+++ src/ui/pages/QueryPage.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { Props } from 'react';
 import {
   Drawer,
   List,
   ListItem,
+  Collapse,
   ListItemIcon,
   ListItemText,
-  Accordion,
-  AccordionSummary,
   Typography,
   AccordionDetails,
+  ListSubheader,
+  ListItemButton,
 } from '@mui/material';
 import { useSelector } from 'react-redux';
 import _ from 'lodash';
-import { selectDatabasesWithCollections } from '../store';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+
+import { Collection, selectDatabasesWithCollections } from '../store';
+
+function ExpandableListItemButton({ databaseName, collections }: { databaseName: string; collections: Collection[] }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <React.Fragment key={databaseName}>
+      <ListItemButton onClick={handleClick}>
+        <ListItemText primary={databaseName} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List dense>
+          {collections.map(({ name: collectionName }) => (
+            <ListItem sx={{ pl: 4 }} button key={`${databaseName}-${collectionName}`}>
+              <ListItemText primary={collectionName} />
+            </ListItem>
+          ))}
+        </List>
+      </Collapse>
+    </React.Fragment>
+  );
+}
 
 export default function QueryPage() {
   const databasesWithCollections = useSelector(selectDatabasesWithCollections);
@@ -30,22 +60,11 @@ export default function QueryPage() {
       variant="permanent"
       anchor="left"
     >
-      {databasesWithCollections.map(({ name: databaseName, collections }) => (
-        <Accordion key={databaseName}>
-          <AccordionSummary>
-            <Typography>{databaseName}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <List dense>
-              {collections.map(({ name: collectionName }) => (
-                <ListItem button key={`${databaseName}-${collectionName}`}>
-                  <ListItemText primary={collectionName} />
-                </ListItem>
-              ))}
-            </List>
-          </AccordionDetails>
-        </Accordion>
-      ))}
+      <List dense subheader={<ListSubheader component="div">Databases</ListSubheader>}>
+        {databasesWithCollections.map(({ name: databaseName, collections }) => (
+          <ExpandableListItemButton key={databaseName} databaseName={databaseName} collections={collections} />
+        ))}
+      </List>
       {/* <List dense>
         {databasesWithCollections.map(({ name }) => (
           <ListItem button key={name}>
